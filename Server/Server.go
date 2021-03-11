88a7c4d423246f3d6b7768f5c72fdb0e1e63b2f0
Alexander Duvall <alexandersantosduvall@gmail.com>
2021-03-11 17:08:36 -0600

    Can now connect Between Markus and App.

--- Server/Server.go
+++ Server/Server.go
@@ -5,21 +5,28 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 type SensorData struct {
-	Temp   float32
-	Height float32
-	Dead   bool
+	Temperature float64
+	Humidity    float64
+	Altitude    float64
+	Pressure    float64
+	Latitude    float64
+	Longitude   float64
 }
 type JetsonData struct {
 	Command    int32
 	Identifier int32
 }
 
+var data SensorData
+
 func sensorData(w http.ResponseWriter, r *http.Request) {
 	_ = r.ParseForm()
-	bytes, err := json.Marshal(SensorData{20.0, -3.4, false})
+	fmt.Println("Gavin is trying to talk!")
+	bytes, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -28,6 +35,16 @@ func sensorData(w http.ResponseWriter, r *http.Request) {
 }
 func JetsonDAta(w http.ResponseWriter, r *http.Request) {
 	_ = r.ParseForm()
+	t, _ := strconv.ParseFloat(r.Form.Get("temp"), 64)
+	humidity, _ := strconv.ParseFloat(r.Form.Get("hum"), 64)
+	altitude, _ := strconv.ParseFloat(r.Form.Get("alt"), 64)
+	pressure, _ := strconv.ParseFloat(r.Form.Get("pre"), 64)
+	lattitude, _ := strconv.ParseFloat(r.Form.Get("lat"), 64)
+	longitude, _ := strconv.ParseFloat(r.Form.Get("lon"), 64)
+	data = SensorData{t, humidity, altitude,
+		pressure, lattitude, longitude}
+	fmt.Println(data)
+
 	bytes, err := json.Marshal(JetsonData{12, 123123})
 	if err != nil {
 		fmt.Println(err)
@@ -37,7 +54,7 @@ func JetsonDAta(w http.ResponseWriter, r *http.Request) {
 }
 func startwebserver(ip []string) {
 	http.HandleFunc("/requestAppData", sensorData)
-	http.HandleFunc("/requestJetsonData", JetsonDAta)
+	http.HandleFunc("/uploadJetsonData", JetsonDAta)
 	fmt.Printf("Starting server at %s:%s\n", ip[0], ip[1])
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%s", ip[0], ip[1]), nil); err != nil {
 		fmt.Println(err)
